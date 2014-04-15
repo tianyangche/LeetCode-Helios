de4fbf42f6f5f155be216561504943fc1072b953
specialCong <specialCong@users.noreply.github.com>
2014-04-15 11:29:52 -0500

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -3,10 +3,10 @@ public class Solution {
         int result[] = new int[2];
         HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
         
-        for(int i=0; i < numbers.length; i ++)
+        for(int i : numbers)
             hm.put(numbers[i], i + 1);
         
-        for(int i = 0; i < numbers.length; i++){
+        for(int i : numbers){
             result[0] = i + 1;
             if(hm.containsKey(target - numbers[i]) && hm.get(target - numbers[i])!=result[0]){
                 result[1] = hm.get(target - numbers[i]);
